c06778fb45e598fc724bc49b6fdfe4bfba4cabb2
little-yellow-bean <283410772@qq.com>
2024-12-26 13:26:53 +1000

    update

--- src/modules/batch-core/job/simple-job.ts
+++ src/modules/batch-core/job/simple-job.ts
@@ -3,12 +3,9 @@ import { Job } from './job';
 import { ExecutionStatus, JobExecution } from '../execution';
 
 export class SimpleJob extends Job {
-  private id: string;
-
   override async execute(): Promise<JobExecution> {
-    this.id = uuid();
     const jobExecution = new JobExecution()
-      .setId(this.id)
+      .setId(uuid())
       .setCreateTime(new Date())
       .setJobParameters(this.parameters)
       .setName(this.name)
@@ -17,66 +14,60 @@ export class SimpleJob extends Job {
     try {
       await this.jobRepository.saveJobExecution(jobExecution);
       await this.notifyListenersBeforeJob(jobExecution);
-      await this.jobRepository.updateJobExecution(
-        jobExecution
-          .setStatus(ExecutionStatus.STARTING)
-          .setLastUpdatedTime(new Date()),
-      );
+      await this.jobRepository.updateJobExecutionById(jobExecution.getId(), {
+        status: ExecutionStatus.STARTING,
+        lastUpdatedTime: new Date(),
+      });
       // TODO: Add pre-started works in the future
-      await this.jobRepository.updateJobExecution(
-        jobExecution
-          .setStatus(ExecutionStatus.STARTED)
-          .setLastUpdatedTime(new Date()),
-      );
+      await this.jobRepository.updateJobExecutionById(jobExecution.getId(), {
+        status: ExecutionStatus.STARTED,
+        lastUpdatedTime: new Date(),
+      });
 
-      this.processSteps().catch(async (error) => {
-        await this.jobRepository.updateJobExecution(
-          jobExecution
-            .setStatus(ExecutionStatus.FAILED)
-            .setEndTime(new Date())
-            .setLastUpdatedTime(new Date())
-            .addFailureException(error),
-        );
+      this.processSteps(jobExecution.getId()).catch(async (error) => {
+        await this.jobRepository.updateJobExecutionById(jobExecution.getId(), {
+          status: ExecutionStatus.FAILED,
+          endTime: new Date(),
+          lastUpdatedTime: new Date(),
+          failureExceptions: [error.message],
+        });
         await this.notifyListenersOnError(jobExecution, error);
         this.logger.error(`Job ${this.name} failed: ${error}`);
       });
       return jobExecution;
     } catch (error) {
-      await this.jobRepository.updateJobExecution(
-        jobExecution
-          .setStatus(ExecutionStatus.FAILED)
-          .setEndTime(new Date())
-          .setLastUpdatedTime(new Date())
-          .addFailureException(error),
-      );
+      await this.jobRepository.updateJobExecutionById(jobExecution.getId(), {
+        status: ExecutionStatus.FAILED,
+        endTime: new Date(),
+        lastUpdatedTime: new Date(),
+        failureExceptions: [error.message],
+      });
       this.notifyListenersOnError(jobExecution, error);
       this.logger.error(`Job ${this.name} failed: ${error}`);
       throw error;
     }
   }
 
-  protected async processSteps() {
+  protected async processSteps(jobId: string) {
     let jobExecution: JobExecution;
     for (const step of this.steps) {
-      jobExecution = await this.jobRepository.findJobExecutionById(this.id);
+      jobExecution = await this.jobRepository.findJobExecutionById(jobId);
       if (jobExecution.isStopping()) {
-        await this.jobRepository.updateJobExecution(
-          jobExecution
-            .setStatus(ExecutionStatus.STOPPED)
-            .setEndTime(new Date())
-            .setLastUpdatedTime(new Date()),
-        );
+        await this.jobRepository.updateJobExecutionById(jobExecution.getId(), {
+          status: ExecutionStatus.STOPPED,
+          endTime: new Date(),
+          lastUpdatedTime: new Date(),
+        });
         this.logger.log(`Job ${this.name} is stopped`);
         return;
       }
       await step.execute(jobExecution);
     }
-    await this.jobRepository.updateJobExecution(
-      jobExecution
-        .setStatus(ExecutionStatus.COMPLETED)
-        .setEndTime(new Date())
-        .setLastUpdatedTime(new Date()),
-    );
+    await this.jobRepository.updateJobExecutionById(jobExecution.getId(), {
+      status: ExecutionStatus.COMPLETED,
+      endTime: new Date(),
+      lastUpdatedTime: new Date(),
+    });
     await this.notifyListenersAfterJob(jobExecution);
     this.logger.log(`Job ${this.name} completed successfully`);
   }
