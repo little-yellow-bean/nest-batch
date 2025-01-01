cb80cd567abff2fc97f5fda764f9634338ff423d
little-yellow-bean <283410772@qq.com>
2025-01-01 11:48:09 +1000

    update

--- src/modules/batch-core/job/job.ts
+++ src/modules/batch-core/job/job.ts
@@ -1,8 +1,9 @@
 import { Logger } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 import { JobListener } from '../listener';
 import { Step } from '../step';
 import { JobRepository } from '../repository';
-import { JobExecution } from '../execution';
+import { ExecutionStatus, JobExecution } from '../execution';
 import { LIB_NAME } from '../constants';
 
 export abstract class Job {
@@ -12,7 +13,81 @@ export abstract class Job {
   protected jobRepository: JobRepository;
   protected readonly logger = new Logger(LIB_NAME);
 
-  abstract execute(parameters: Record<string, any>): Promise<JobExecution>;
+  async execute(parameters: Record<string, any>): Promise<JobExecution> {
+    if (!this.name?.trim().length) {
+      throw new Error('Job name is required');
+    }
+    if (!this.jobRepository) {
+      throw new Error('Job repository is required');
+    }
+
+    let jobExecution = new JobExecution()
+      .setId(uuid())
+      .setCreateTime(new Date())
+      .setJobParameters(parameters)
+      .setName(this.name)
+      .transitionStatus(ExecutionStatus.CREATED)
+      .setLastUpdatedTime(new Date());
+
+    try {
+      jobExecution = await this.jobRepository.saveJobExecution(jobExecution);
+      await this.notifyListenersBeforeJob(jobExecution);
+      jobExecution = await this.jobRepository.updateJobExecution(
+        jobExecution
+          .transitionStatus(ExecutionStatus.STARTING)
+          .setStartTime(new Date())
+          .setLastUpdatedTime(new Date()),
+      );
+      // TODO: Add pre-started works in the future
+      jobExecution = await this.jobRepository.updateJobExecution(
+        jobExecution
+          .transitionStatus(ExecutionStatus.STARTED)
+          .setLastUpdatedTime(new Date()),
+      );
+
+      this.processSteps(jobExecution.getId())
+        .then(async (status) => {
+          jobExecution = await this.jobRepository.updateJobExecution(
+            jobExecution
+              .transitionStatus(status)
+              .setEndTime(new Date())
+              .setLastUpdatedTime(new Date()),
+          );
+          if (status === ExecutionStatus.STOPPED) {
+            this.logger.log(`Job ${this.name} is stopped`);
+          }
+          if (status === ExecutionStatus.COMPLETED) {
+            await this.notifyListenersAfterJob(jobExecution);
+            this.logger.log(`Job ${this.name} completed successfully`);
+          }
+        })
+        .catch(async (error: Error) => {
+          await this.jobRepository.updateJobExecution(
+            jobExecution
+              .transitionStatus(ExecutionStatus.FAILED)
+              .setEndTime(new Date())
+              .setLastUpdatedTime(new Date())
+              .setFailureExceptions([error.message]),
+          );
+          await this.notifyListenersOnError(jobExecution, error);
+          this.logger.error(`Job ${this.name} failed: ${error}`);
+        });
+      return jobExecution;
+    } catch (error) {
+      await this.jobRepository.updateJobExecution(
+        jobExecution
+          .transitionStatus(ExecutionStatus.FAILED)
+          .setEndTime(new Date())
+          .setLastUpdatedTime(new Date())
+          .setFailureExceptions([error.message]),
+      );
+      await this.notifyListenersOnError(jobExecution, error);
+      this.logger.error(`Job ${this.name} failed: ${error}`);
+      throw error;
+    }
+  }
+
+  abstract processSteps(jobId: string): Promise<ExecutionStatus>;
 
   getSteps() {
     return this.steps;
--- src/modules/batch-core/job/simple-job.ts
+++ src/modules/batch-core/job/simple-job.ts
@@ -1,91 +1,16 @@
-import { v4 as uuid } from 'uuid';
 import { Job } from './job';
 import { ExecutionStatus, JobExecution } from '../execution';
 
 export class SimpleJob extends Job {
-  override async execute(
-    parameters: Record<string, any>,
-  ): Promise<JobExecution> {
-    if (!this.name?.trim().length) {
-      throw new Error('Job name is required');
-    }
-    if (!this.jobRepository) {
-      throw new Error('Job repository is required');
-    }
-
-    let jobExecution = new JobExecution()
-      .setId(uuid())
-      .setCreateTime(new Date())
-      .setJobParameters(parameters)
-      .setName(this.name)
-      .transitionStatus(ExecutionStatus.CREATED)
-      .setLastUpdatedTime(new Date());
-
-    try {
-      jobExecution = await this.jobRepository.saveJobExecution(jobExecution);
-      await this.notifyListenersBeforeJob(jobExecution);
-      jobExecution = await this.jobRepository.updateJobExecution(
-        jobExecution
-          .transitionStatus(ExecutionStatus.STARTING)
-          .setStartTime(new Date())
-          .setLastUpdatedTime(new Date()),
-      );
-      // TODO: Add pre-started works in the future
-      jobExecution = await this.jobRepository.updateJobExecution(
-        jobExecution
-          .transitionStatus(ExecutionStatus.STARTED)
-          .setLastUpdatedTime(new Date()),
-      );
-
-      this.processSteps(jobExecution.getId()).catch(async (error: Error) => {
-        await this.jobRepository.updateJobExecution(
-          jobExecution
-            .transitionStatus(ExecutionStatus.FAILED)
-            .setEndTime(new Date())
-            .setLastUpdatedTime(new Date())
-            .setFailureExceptions([error.message]),
-        );
-        await this.notifyListenersOnError(jobExecution, error);
-        this.logger.error(`Job ${this.name} failed: ${error}`);
-      });
-      return jobExecution;
-    } catch (error) {
-      await this.jobRepository.updateJobExecution(
-        jobExecution
-          .transitionStatus(ExecutionStatus.FAILED)
-          .setEndTime(new Date())
-          .setLastUpdatedTime(new Date())
-          .setFailureExceptions([error.message]),
-      );
-      await this.notifyListenersOnError(jobExecution, error);
-      this.logger.error(`Job ${this.name} failed: ${error}`);
-      throw error;
-    }
-  }
-
-  protected async processSteps(jobId: string) {
+  override async processSteps(jobId: string) {
     let jobExecution: JobExecution;
     for (const step of this.steps) {
       jobExecution = await this.jobRepository.findJobExecutionById(jobId);
       if (jobExecution.isStopping()) {
-        await this.jobRepository.updateJobExecution(
-          jobExecution
-            .transitionStatus(ExecutionStatus.STOPPED)
-            .setEndTime(new Date())
-            .setLastUpdatedTime(new Date()),
-        );
-        this.logger.log(`Job ${this.name} is stopped`);
-        return;
+        return ExecutionStatus.STOPPED;
       }
       await step.execute(jobExecution);
     }
-    await this.jobRepository.updateJobExecution(
-      jobExecution
-        .transitionStatus(ExecutionStatus.COMPLETED)
-        .setEndTime(new Date())
-        .setLastUpdatedTime(new Date()),
-    );
-    await this.notifyListenersAfterJob(jobExecution);
-    this.logger.log(`Job ${this.name} completed successfully`);
+    return ExecutionStatus.COMPLETED;
   }
 }
