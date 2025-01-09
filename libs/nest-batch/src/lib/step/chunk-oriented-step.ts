108741a088630af752fc399d7c1a47dd1cda6d7a
little-yellow-bean <283410772@qq.com>
2025-01-10 09:56:53 +1000

    fix

--- libs/nest-batch/src/lib/step/chunk-oriented-step.ts
+++ libs/nest-batch/src/lib/step/chunk-oriented-step.ts
@@ -4,22 +4,24 @@ import { chunkArray } from '../utils';
 export class ChunkOrientedStep<I, O> extends Step<I, O> {
   override async processItems() {
     const items = await this.reader.read();
-    const processedItems: O[] = [];
+    if (items) {
+      const processedItems: O[] = [];
 
-    for (const item of items) {
-      if (this.processor) {
-        const processedItem = await this.processor.process(item);
-        if (processedItem != null) {
-          processedItems.push(processedItem);
+      for (const item of items) {
+        if (this.processor) {
+          const processedItem = await this.processor.process(item);
+          if (processedItem != null) {
+            processedItems.push(processedItem);
+          }
+        } else {
+          processedItems.push(item as unknown as O);
         }
-      } else {
-        processedItems.push(item as unknown as O);
       }
-    }
 
-    const chunks = chunkArray(processedItems, this.chunkSize);
-    for (const chunk of chunks) {
-      await this.writer.write(chunk);
+      const chunks = chunkArray(processedItems, this.chunkSize);
+      for (const chunk of chunks) {
+        await this.writer.write(chunk);
+      }
     }
   }
 }
